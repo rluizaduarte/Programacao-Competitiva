63f72970988f8f9e0dede1a974a91c080e7c8f82
rluizaduarte <riellyduarte75@gmail.com>
2025-05-22 11:34:35 -0300

    CorruptedImage WA on test 2

--- CodeForces/CorruptedImages/101532H.cpp
+++ CodeForces/CorruptedImages/101532H.cpp
@@ -10,38 +10,43 @@ int main(){
 		
 		int nDeUnsNecessario = (n * 2 + m * 2) - 4;
 		// pra ser uma imagem nao corrompida, precisa ter pelo menosas bordas iguas a um
-		int nDeUnsNaImagem = 0;
-		// contador que diz quantos uns há
-
-		int zerosNaBorda = 0;
-		int unsNaBorda = 0;
-		// contador que diz quantos movimentos serão necessários
 
 		int imagem[n][m];
 		for(int j = 0; j < n; j++){
 			int linha;
 			cin >> linha;
-			for(int k = m; k > -1; k--){
+			for(int k = (m - 1); k > -1; k--){
 				imagem[j][k] = linha % 10;
 				linha /= 10;
 				// preenchendo a matriz com um digito por vez
 				// do ultimo digito até o primeiro
+			}
+		}
 
+		int nDeUnsNaImagem = 0;
+		// contador que diz quantos uns há
+		int zerosNaBorda = 0;
+		int unsNaBorda = 0;
+		// contador que diz quantos movimentos serão necessários
+		
+		for(int j = 0; j < n; j++){
+			for(int k = 0; k < m; k++){
 				if(imagem[j][k] == 1){
 					nDeUnsNaImagem++;
-					// verifica quantos uns há na imagem
 				}
+				// verifica quantos uns há na imagem
 
 				if(j == 0 || j == (n - 1) || k == 0 || k == (m - 1)){
 					if(imagem[j][k] == 0){
 						zerosNaBorda++;	
 					}else{
 						unsNaBorda++;
-					// verifica quantos zeros e quantos uns há na borda
 					}
 				}
+				// verifica quantos zeros e quantos uns há na borda
 			}
 		}
+		
 		int movimentos;
 		if(nDeUnsNaImagem < nDeUnsNecessario){
 			movimentos = -1;
