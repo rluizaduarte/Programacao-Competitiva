99c2f2b3160b8d8961a576e9d9c75850b6dafb4e
rluizaduarte <riellyduarte75@gmail.com>
2025-07-22 12:02:12 -0300

    22/07

--- CodeForces/Watermelon/4A.cpp
+++ CodeForces/Watermelon/4A.cpp
@@ -6,9 +6,9 @@ int main(){
 	cin >> w;
     
 	if(w % 2 == 0){
-        	cout << "YES";
+        	cout << "YES" << endl;
     	}else{
-        	cout << "NO";
+        	cout << "NO" << endl;
     	}
     
 	return 0;
