8ad4c5febff59a6ec8c83afec2c6bd9b5deacae4
Rielly Luiza <riellyduarte75@gmail.com>
2025-08-13 17:01:10 -0300

    Contest 13/08

---
+++ UVA/LetMeCountTheWays/357.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define MAX 100000
+
+using namespace std;
+
+
+typedef long long ll;
+ll ways[MAX];
+int coins[] = {1, 5, 10, 25, 50};
+
+void coinChange(){
+    ways[0] = 1;
+    
+    for (auto c : coins){
+        for (int i = c; i <= MAX; i++){
+            ways[i] += ways[i-c];
+        }
+    }
+}
+
+int main(){
+    coinChange();
+    int n;
+    while(cin >> n){
+        if(n < 5){
+            cout << "There is only 1 way to produce " << n << " cents change." << endl;
+        }else{
+            cout << "There are " << ways[n] << " ways to produce " << n << " cents change." << endl;
+        }
+    }
+}
