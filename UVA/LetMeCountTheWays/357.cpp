98dc720cc169cf9b4c4c0804a59549001b08c683
Rielly Luiza <riellyduarte75@gmail.com>
2025-08-13 17:15:49 -0300

    Contest 13/08

--- UVA/LetMeCountTheWays/357.cpp
+++ UVA/LetMeCountTheWays/357.cpp
@@ -1,16 +1,13 @@
 #include <bits/stdc++.h>
 #define MAX 100000
-
 using namespace std;
-
 
 typedef long long ll;
 ll ways[MAX];
 int coins[] = {1, 5, 10, 25, 50};
 
 void coinChange(){
-    ways[0] = 1;
-    
+    ways[0] = 1;    
     for (auto c : coins){
         for (int i = c; i <= MAX; i++){
             ways[i] += ways[i-c];
